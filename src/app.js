b23b221dd5e0411e5d13192cf64f31d39f69c3f3
CodingByMoMo <bartek@momocodes.com>
2023-11-02 22:57:08 +0100

    Setup a Proxy for React App

--- src/app.js
+++ src/app.js
@@ -62,6 +62,14 @@ app.use(function (req, res, next) {
 	next(createError(404));
 });
 
+if (process.env.NODE_ENV === 'production') {
+	app.use(express.static('../client/bulid'));
+
+	app.get('*', (req, res) => {
+		res.sendFile(path.resolve(__dirname, 'client', 'build', 'index.html'));
+	});
+}
+
 // error handler
 app.use(function (err, req, res, next) {
 	// set locals, only providing error in development
